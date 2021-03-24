52a6f49b05d6822cbf495d82843b003fbce067bc
Seth Delorme <delorme.s.m@gmail.com>
2021-03-23 20:20:46 -0500

    creates products index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      {{ product.name }}
+      <hr />
+    </div>
   </div>
 </template>
 <style></style>
 <script>
+import axios from "axios";
+
 export default {
-  data: function() {
+  data: function () {
     return {
-      message: "Welcome to Vue.js! Where all your dreams come true...."
+      message: "Welcome to Vue.js! Where all your dreams come true....",
+      products: [],
     };
   },
-  // methods: {
-  //   makeDreamsComeTrue: function () {},
-  // },
-  created: function() {},
-  methods: {},
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
