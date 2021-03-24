c065e3bb064220df0a6b67e444bbf5276a6c9c77
Seth Delorme <delorme.s.m@gmail.com>
2021-03-23 19:20:33 -0500

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      message: "Welcome to Vue.js! Where all your dreams come true...."
+    };
   },
+  // methods: {
+  //   makeDreamsComeTrue: function () {},
+  // },
+  created: function() {},
+  methods: {},
 };
 </script>
