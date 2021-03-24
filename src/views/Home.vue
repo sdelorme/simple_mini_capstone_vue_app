ce59be6736d781408d7f62f8c650843e87886b4e
Seth Delorme <delorme.s.m@gmail.com>
2021-03-23 21:04:03 -0500

    adds create action fully

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p><button v-on:click="createProduct">Create New Product</button></p>
+    <p>
+      Name
+      <input v-model="newProductName" />
+    </p>
+    <p>
+      Description
+      <input v-model="newProductDescription" />
+    </p>
+    <p>
+      Price
+      <input v-model="newProductPrice" />
+    </p>
+    <p>
+      Image Url
+      <input v-model="newProductImageUrl" />
+    </p>
     <div v-for="product in products" v-bind:key="product.id">
+      <hr />
       {{ product.name }}
       <hr />
     </div>
@@ -16,6 +34,10 @@ export default {
     return {
       message: "Welcome to Vue.js! Where all your dreams come true....",
       products: [],
+      newProductName: "",
+      newProductDescription: "",
+      newProductPrice: "",
+      newProductImageUrl: "",
     };
   },
   created: function () {
@@ -26,6 +48,23 @@ export default {
       axios.get("http://localhost:3000/api/products").then((response) => {
         console.log(response.data);
         this.products = response.data;
+      });
+    },
+    createProduct: function () {
+      console.log("about to create this product though...");
+      var params = {
+        name: this.newProductName,
+        description: this.newProductDescription,
+        price: this.newProductPrice,
+        image_url: this.newProductImageUrl,
+      };
+      axios.post("http://localhost:3000/api/products", params).then((response) => {
+      console.log(response.data);
+      this.products.push(response.data);
+      this.newProductName = "";
+      this.newProductDescription = "";
+      this.newProductPrice = "";
+      this.newProductImageUrl = "";
       });
     },
   },
